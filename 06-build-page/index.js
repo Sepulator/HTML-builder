e25675d31deff81d66e30337404fc818e1ff5509
Yuri Skrypal <yjura@protonmail.com>
2025-01-20 12:59:41 +0200

    feat: copy all modules in one file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,5 @@
-const { mkdir, readdir } = require('fs/promises');
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
 const { createReadStream, createWriteStream } = require('fs');
-const mergeStyles = require('../05-merge-styles');
-const copyDir = require('../04-copy-directory');
 const path = require('path');
 
 const outPath = path.join(__dirname, 'project-dist');
@@ -12,8 +10,9 @@ const srcAssetsPath = path.join(__dirname, 'assets');
 const outAssetsPath = path.join(__dirname, 'project-dist', 'assets');
 const indexPath = path.join(__dirname, 'project-dist', 'index.html');
 const componentsPath = path.join(__dirname, 'components');
-const options = { withFileTypes: true };
+const options = { withFileTypes: true, recursive: true };
 
+/** @param {string} filePath  */
 const getContent = async (filePath) => {
   let data = '';
   return new Promise((res, rej) => {
@@ -22,6 +21,50 @@ const getContent = async (filePath) => {
       .on('end', () => res(data))
       .on('error', rej);
   });
+};
+
+/**
+ * @param {string} srcPath
+ * @param {string} outPath
+ */
+const mergeStyles = async (srcPath, outPath) => {
+  const output = createWriteStream(outPath);
+  try {
+    const files = await readdir(srcPath, options);
+    for (const file of files) {
+      const filePath = path.join(file.path, file.name);
+      const { ext } = path.parse(filePath);
+      if (file.isFile() && ext === '.css') {
+        const input = createReadStream(filePath);
+        input.pipe(output);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/**
+ * @param {string} srcDirPath
+ * @param {string} outDirPath
+ **/
+const copyDir = async (srcDirPath, outDirPath) => {
+  await rm(outDirPath, { recursive: true, force: true });
+  const files = await readdir(srcDirPath, options);
+  await mkdir(outDirPath, { recursive: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      const destPath = path.join(outDirPath, file.name);
+      await mkdir(destPath, { recursive: true });
+    }
+
+    if (file.isFile()) {
+      const rel = file.path.split(srcDirPath)[1];
+      const srcFile = path.join(file.path, file.name);
+      const destFile = path.join(outDirPath, rel, file.name);
+      await copyFile(srcFile, destFile);
+    }
+  }
 };
 
 const buildPage = async () => {
